1fa2494601dc6be10d30221c111d200027f78f65
Gustavo Oliveira <gustavo.oliveira@avantare.intranet>
2019-01-03 15:24:29 -0200

    Updates

---
+++ precache-manifest.576ac0cfc3a1b1213304afa6e2548b6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65f45a3ce960647d094",
+    "url": "/marvel-redux/static/js/runtime~main.a65f45a3.js"
+  },
+  {
+    "revision": "93d546718237b484f0ea",
+    "url": "/marvel-redux/static/js/main.93d54671.chunk.js"
+  },
+  {
+    "revision": "adeef247933413bd2c4f",
+    "url": "/marvel-redux/static/js/1.adeef247.chunk.js"
+  },
+  {
+    "revision": "93d546718237b484f0ea",
+    "url": "/marvel-redux/static/css/main.8a5ee7b4.chunk.css"
+  },
+  {
+    "revision": "356015f0e004a5516fa48d91fd353be1",
+    "url": "/marvel-redux/index.html"
+  }
+];
